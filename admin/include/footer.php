64737823908b7fee786ac1c20bc30c52bc47c9d5
shazib-ahmed <shajibahmed420@gmail.com>
2023-11-13 11:05:23 +0600

    Food ordering system in php

---
+++ admin/include/footer.php
@@ -0,0 +1,1 @@
+ <footer class="footer" style="text-align: center;">Shazib Ahmed © 2023 - Online Food Ordering System Developer - <a href="https://www.facebook.com/shazibahmed.rq">Shazib Ahmed</a> </footer>
